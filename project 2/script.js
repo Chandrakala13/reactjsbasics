7e512e2542b2684bc6ca13bd2d5d825b549d2c84
Chandrakala13 <shettychandhushetty@gmail.com>
2026-04-16 12:53:30 -0700

    react js basics

---
+++ project 2/script.js
@@ -0,0 +1,10 @@
+function CalculateGST(price){
+    return price * 0.18;
+}
+document.getElementById("btn").addEventListener("click", 
+    function() {
+        var price = document .getElementById("price").value;
+        let gst = CalculateGST(price);
+        document.getElementById("result").innerText = "Total Price: " + (Number(price) + Number(gst));
+    });
+    
